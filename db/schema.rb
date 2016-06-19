61826b12a1957e717fce87afe2fdb3393cd11ad6
Nolan Morache <nolan241@gmail.com>
2016-06-19 04:12:10 +0000

    Add username to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160618213526) do
+ActiveRecord::Schema.define(version: 20160619034808) do
 
   create_table "listings", force: :cascade do |t|
     t.string   "title"
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 20160618213526) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
